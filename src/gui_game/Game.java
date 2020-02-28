43461cceffc7889128d76d3ac5a6f6747ca33be6
yeonsu100 <yeonsu100@gmail.com>
2020-02-28 19:57:55 +0900

    Finished a Number Slide Puzzle game (5*5 version)

---
+++ src/gui_game/Game.java
@@ -0,0 +1,17 @@
+package gui_game;
+
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+		JFrame frame=new JFrame();
+		frame.setTitle("Slide Game");
+		frame.setSize(1000, 800);
+		frame.setLocation(100, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		NumberSlidePuzzle nsp=new NumberSlidePuzzle();
+		frame.add(nsp);
+		frame.revalidate();
+	}
+}
